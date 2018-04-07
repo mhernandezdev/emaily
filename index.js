04e874545d11339017e070009808c60907702247
Mike Hernandez <bad.fish.mh@gmail.com>
2018-04-07 07:54:55 -0400

    changed greeting

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'})
+    res.send({ bye: 'buddy'})
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// https://frozen-gorge-83511.herokuapp.com/
+// https://git.heroku.com/frozen-gorge-83511.herokuapp.git
