82c429f82d677e54c35e93f90d1b8739712f1eda
Mike Hernandez <bad.fish.mh@gmail.com>
2018-04-15 08:38:07 -0400

    keys fix

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,5 @@ if(process.env.NODE_ENV === 'production'){
     module.exports = require('./prod');
 }else{
     console.log('WHY AM I IN DEV')
-    module.exports = require('./dev');
+    //module.exports = require('./dev');
 }
